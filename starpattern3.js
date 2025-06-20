e418b8b3ee2bba469d732ebe4af4a5ac74decffc
Sandeep K Mohanty <83744666+Sandeepmohanty01@users.noreply.github.com>
2025-06-20 08:47:03 +0530

    Create starpattern3.js

---
+++ starpattern3.js
@@ -0,0 +1,14 @@
+for( let i = 1; i <= 5; i++){
+    pattern = ""
+    for( let j = 1; j <= i; j++){
+        pattern = pattern + j;
+        
+    }
+    console.log(pattern);
+}
+
+1
+12
+123
+1234
+12345
