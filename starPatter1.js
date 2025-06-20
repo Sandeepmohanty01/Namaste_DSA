7b928a333098955210d5006dc0c3ac36ef4fa76f
Sandeep K Mohanty <83744666+Sandeepmohanty01@users.noreply.github.com>
2025-06-20 08:27:46 +0530

    Create starPatter1.js

---
+++ starPatter1.js
@@ -0,0 +1,13 @@
+output:
+****
+****
+****
+****
+
+for( let i = 0; i < 4; i++){
+    let row = "";
+    for( let j = 0; j < 4; j++){
+        row = row + "*";       
+    }
+    console.log(row);
+}
